d34c5dbeceb5b69286bf93239e081aa72bc8306e
slavikp <slavik.pashanin@gmail.com>
2016-07-30 13:24:44 +0300

    Q_1 update

--- mmn_11/Q_1.sql
+++ mmn_11/Q_1.sql
@@ -1,12 +1,12 @@
 create table Lawyer
-(lname varchar(40),
+(lname varchar(40) not Null,
 sdate date,
 hbilling integer,
 partner date,
 primary key (lname));
 
 create table File
-(fid numeric(7,0),
+(fid numeric(7,0) not Null,
 fname varchar (100),
 description varchar (1024), 
 status date,
@@ -15,25 +15,24 @@ lname varchar(40),
 primary key (fid));
 
 create table Client
-(cname varchar(40),
+(cname varchar(40) not Null,
 tel char(11),
 address varchar (50),
 primary key (cname));
 
 create table Billing
-(fid numeric(7,0),
-bdate date,
-lname varchar(40),
+(fid numeric(7,0) not Null,
+bdate date not Null,
+lname varchar(40) not Null,
 hours integer,
 descr varchar (100), 
 primary key (fid, bdate, lname),
 foreign key (fid) references File,
 foreign key (lname) references Lawyer);
 
-
 create table Lonfile
-(fid numeric(7,0),
-lname varchar (40),
+(fid numeric(7,0) not Null,
+lname varchar (40) not Null,
 responsibility varchar (50),
 primary key (fid, lname),
 foreign key (fid) references File,
