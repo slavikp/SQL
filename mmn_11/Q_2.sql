4c0a00d6667c0ff22086a1277c8d2ca3df9594a8
slavikp <slavik.pashanin@gmail.com>
2016-08-03 10:58:06 +0300

    update 3/8

--- mmn_11/Q_2.sql
+++ mmn_11/Q_2.sql
@@ -7,9 +7,27 @@ execute procedure trigf();
 
 --------function-------------------
 create or replace function trigf()  returns trigger as $$
-Declare r record
-brgin
+Declare lawyerWorkOnCase  integer;
+begin
+	select count(lname) as countLawyer
+	from
+	(select lname
+	from file
+	where fid = new.fid  and lname = new.lname
+	union 
+	select lname
+	from lonfile
+	where fid = new.fid  and lname = new.lname) as lawyers into lawyerWorkOnCase;
 
+	if (lawyerWorkOnCase > 0)
+	then 
+		return new;
+	else	
+		raise notice 'lawyer is not work in this case';
+		if (tg_op ='UPDATE') then return old;
+		elsif (tg_op='INSERT') then return Null;
+		else return Null;
+		end if;
+	end if;
 
 End; $$ LANGUAGE plpgsql;
-
